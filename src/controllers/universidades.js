02754b0b37cf694a713a2d20b12dbe8c35199ad3
PamelaCardenas <pamela.cardenas99@gmail.com>
2021-11-21 21:47:28 -0600

    Creacion de archivo en directorio seeders

--- src/controllers/universidades.js
+++ src/controllers/universidades.js
@@ -3,8 +3,8 @@
 const db = require('../models')
 
 exports.getAllUniversidades = async (req, res, next) => {
-    const allUniversidades = await db.Universidades.findAll({
-        attributes: ['id', 'nombre', 'ciudad', 'direccion', 'añoinicio', 'fechacreacion'],
+    const allUniversidades = await db.universidades.findAll({
+        attributes: ['id', 'nombre', 'ciudad', 'dirección', 'añoinicio', 'fechacreacion'],
         })
         res.status(200).send(allUniversidades)
 }
---
+++ src/models/seeders/20211122033546-demo-universidades.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'universidades',
+      [
+        {
+          nombre:'Universidad Autónoma de Chihuahua',
+          ciudad: 'Chihuahua',
+          dirección: 'Av. Universidad y Av. Pascual Orozco',
+          añoinicio: '1835',
+          fechacreacion: new Date()
+        },
+        {
+          nombre:'Universidad Tecnologica de Chihuahua',
+          ciudad: 'Chihuahua',
+          dirección: 'Av. Montes Americanos',
+          añoinicio: '2000',
+          fechacreacion: new Date()
+        },
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('usuarios', null, {})
+  },
+};
