0c41bbcc104f7bd68a86ebdfec58171f9fda11f2
PamelaCardenas <pamela.cardenas99@gmail.com>
2021-12-08 20:09:09 -0600

    Ajustes en config.json

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const db = require('./models')
 const createSeeds = require('./models/seeders')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
